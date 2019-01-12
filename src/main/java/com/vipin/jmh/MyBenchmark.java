52e17c07eb4e0a8a9b7e2ef0397f6ce0adf7c325
Vipin Kumar Sharma <vipinsharma85@gmail.com>
2019-01-12 23:21:23 +0530

    Initial commit

---
+++ src/main/java/com/vipin/jmh/MyBenchmark.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vipin.jmh;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import cern.colt.list.DoubleArrayList;
+import cern.jet.stat.Descriptive;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+public class MyBenchmark {
+
+    /*@Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+	List<Integer> lst = IntStream.rangeClosed(1,1000000).boxed().collect(Collectors.toList());
+
+        int total = lst.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println(total);
+    }*/
+
+    /*public static void main(String[] args) {
+        new MyBenchmark().testInversionSumForLoop();
+        new MyBenchmark().testInversionSumUsingStreams();
+        new MyBenchmark().testInversionSumUsingCernColt();
+    }*/
+
+
+    public static double[] array;
+
+    static {
+        int num_of_elements = 100;
+        array = new double[num_of_elements];
+        for (int i = 0; i < num_of_elements; i++) {
+            array[i] = i+1;
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void testInversionSumForLoop(){
+        double result = 0;
+        for (int i = 0; i < array.length; i++) {
+            result += 1.0/array[i];
+        }
+        /*System.out.println("Result testForLoopInversionSum " + result);*/
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void testInversionSumUsingStreams(){
+        double result = 0;
+        result = Arrays.stream(array).map(d -> 1/d).sum();
+        /*System.out.println(Result testInversionSumUsingStreams " + result);*/
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void testInversionSumUsingCernColt(){
+        double result = Descriptive.sumOfInversions(new DoubleArrayList(array), 0, array.length-1);
+        /*System.out.println("Result testInversionSumUsingCernColt " + result);*/
+    }
+
+}
